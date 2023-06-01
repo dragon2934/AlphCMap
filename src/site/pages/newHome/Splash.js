bb75b0783a636dcdc298ec1f7f8e36b3dbfedf07
Long Zhou <dragon2934@gmail.com>
2023-05-31 20:18:37 -0400

    change Splash

--- src/site/pages/newHome/Splash.js
+++ src/site/pages/newHome/Splash.js
@@ -3,13 +3,13 @@ import React from "react";
 const Splash = () => {
     return (
         <div className="full-screen splash-screen isLoading">
-            <h1 className="splash-text">KloserToYou: Infinite Marketing Possibilities</h1>
+            <h1 className="splash-text">KloserToYou</h1>
             <img
                 className="splash-image"
                 src={'/assets/img/logo-alphc.png'}
                 alt={'logo'}
             />
-
+            <h1 className="splash-text">Infinite Marketing Possibilities</h1>
         </div>
     );
 };
