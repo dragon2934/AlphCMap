206913e710746a6f66afcb3c685fce8ab87cb435
Long Zhou <dragon2934@gmail.com>
2023-07-15 20:39:34 -0400

    fix bug

--- src/site/pages/newHome/UnSubscribed.js
+++ src/site/pages/newHome/UnSubscribed.js
@@ -4,19 +4,22 @@ import Footer from './Footer';
 import Header from './Header';
 const UnSubscribed = () => {
     return (
-        <main>
-            <Header />
-            <div className="content terms-of-use">
-                <Container>
+        <Col md={12} sm={12} xs={12} className="overlay-form-container">
+            <Link to={'/'}>
+                <img
+                    className={'logo-container'}
+                    src={'/assets/img/logo-white.png'}
+                    alt={'logo'}
+                />
+            </Link>
+            <br /><br /><br /><br /><br />
+            <p> You're unsubscribed. </p>
 
-                    <p> You're unsubscribed.
-
-                    </p>
-
-                </Container>
-            </div>
-            <Footer />
-        </main>
+            <Row>
+                <Col> UnSubscribe Successful! Sorry to see you go!
+                </Col>
+            </Row>
+        </Col>
     );
 };
 
