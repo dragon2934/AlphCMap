65f1c1978c244426f76d8cf4f7d9d21ebc1732b8
Long Zhou <dragon2934@gmail.com>
2023-09-07 20:52:50 -0400

    fix bug

--- src/site/pages/newHome/BusinessInfo.js
+++ src/site/pages/newHome/BusinessInfo.js
@@ -305,7 +305,7 @@ const BusinessInfo = ({ }) => {
                                             <Col><h3>{companyProfile.companyName}</h3></Col> </Row>
                                         <Row>   <Col><i className="fa-solid fa-address"></i> {(property.unit_no ? property.unit_no + ' - ' : '') + property.street_number + ' ' + property.route + ' ' + property.locality + ',' + property.city + ',' + property.postal_code} </Col> </Row>
                                         <Row>   <Col><i className="fa-solid fa-phone"></i> {companyProfile.phone} </Col> </Row>
-                                        <Row>   <Col><i className="fa-solid fa-globe"></i> <a href={companyProfile.website} className='business_link'>Company Website</a>  </Col> </Row>
+                                        <Row>   <Col><i className="fa-solid fa-globe"></i> <a href={companyProfile.website.startsWith('http') ? companyProfile.website : 'https://' + companyProfile.website} className='business_link'>Company Website</a>  </Col> </Row>
                                         {totalConnected ? <Row>   <Col>Connected: {totalConnected} </Col> </Row> : null}
                                         <Row>   <Col><i className="fa-solid fa-globe"></i> <a className='business_link' href={shareUrl} target="_blank"> {shareUrl}</a>
                                             {
